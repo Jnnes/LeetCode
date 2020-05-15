cfa62601dfd00e92c87181f99a3a59e41757e2c2
wangzl02 <wangzl02@mail.jj.cn>
2020-05-15 14:46:56 +0800

    69_x的平方根添加牛顿迭代法解法

--- leetCode/solution/69_sqrt.h
+++ leetCode/solution/69_sqrt.h
@@ -5,7 +5,7 @@
 class Solution {
 public:
     /*
-        
+        二分法
     */
     int mySqrt(int x) {
         if (x <= 1)
@@ -30,6 +30,28 @@ public:
         }
 
         return left;
+    }
+
+    /*
+        牛顿迭代法，因为可能从大小两个方向渐进，所以需要从两个方向判断
+    */
+    int mySqrtNewton(int a) {
+        if (a <= 1)
+            return a;
+        else if (a <= 4)
+            return a / 2;
+
+        long ret = a;
+
+        while (true) {
+            ret = ret - (ret * ret - a) / ret / 2;
+
+            if (ret * ret <= a && (ret + 1) * (ret + 1) > a)
+                return ret;
+
+            if (ret * ret > a && (ret - 1) * (ret - 1) <= a)
+                return ret - 1;
+        }
     }
 };
 
