c2821573170fccb7cf4cbf701b93ce261470d516
wangzl02 <wangzl02@mail.jj.cn>
2020-05-24 00:16:14 +0800

    添加leetcode刷题框架外的临时测试接口 consoleTest，防止频繁改动main函数

--- leetCode/leetCode.cpp
+++ leetCode/leetCode.cpp
@@ -5,11 +5,24 @@
 
 #include "solution/1371.h"
 
+/*
+    控制台测试函数，防止频繁修改main函数代码进行其他测试
+*/
+void consoleTest();
+
 int main()
 {
+    consoleTest();
+
     Solution so;
     run(so);
     std::cout << std::endl << std::endl << "Hello LeetCode!\n" << std::endl;
 
     return 0;
+}
+
+void consoleTest() {
+    cout << "Run console test!" << endl;
+
+
 }
